887923a3fc0a643af173658885c978ee08d7a953
aswanth <aswanthr18tech@gmail.com>
2024-06-07 12:01:07 +0530

    mongodb url

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,12 @@
 const mongoose=require('mongoose')
-const url = 'mongodb://127.0.0.1:27017/Hoomans';
+// const url = 'mongodb://127.0.0.1:27017/Hoomans';
+const url='mongodb+srv://aswanthrtech:Aswr200018@cluster0.dmaekjp.mongodb.net/Hoomans'
 
 module.exports.connectDB=()=>{
-    mongoose.connect(url,{})
+    mongoose.connect(url,{
+        useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
     .then(()=>{
         console.log('Database connected success');
     })
